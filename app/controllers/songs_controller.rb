4fbfbc2f6c84fd78b0481cad0c34107b19b9fc13
Marc <marc.ferraro1@gmail.com>
2021-01-22 15:45:35 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = set_song
   end
 
   def new
@@ -51,5 +53,9 @@ class SongsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def set_song
+    Song.find(params[:id])
   end
 end
